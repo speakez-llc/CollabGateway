bad45cbbe6d5b78f0d10d46d1151993f4c231ff5
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2024-09-20 11:57:23 -0400

    Set execute permission on build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Install .NET SDK
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet8
 export PATH=./dotnet8:$PATH
-
-# Restore tools and build the project
 dotnet tool restore
 dotnet build
-
-# Run Fable and Vite build
 dotnet fable --run npx vite build
