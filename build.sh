6bdaf4b3c2110f27bef81ccb04f985446f112f9e
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2024-09-20 17:35:28 -0400

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh -c 8.0 -InstallDir ./dotnet8
-export PATH=./dotnet8:$PATH
-dotnet tool restore
-dotnet build
-dotnet fable --run npx vite build
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh; 
+chmod +x dotnet-install.sh; 
+./dotnet-install.sh -c 8.0 -InstallDir ~/dotnet8; 
+export PATH=$PATH:~/dotnet8; 
+dotnet tool restore; 
+dotnet build -c Release; 
