7b50c8dbbb765adfbf0bbb5b41eb10254ba2fc14
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2024-09-23 00:04:23 -0400

    add files - knee-cap Carousel.fs

--- src/CollabGateway.Client/Components/Carousel.fs
+++ src/CollabGateway.Client/Components/Carousel.fs
@@ -3,22 +3,22 @@
 open Feliz
 
 [<ReactComponent>]
-let Carousel images =
+let Carousel (images: string seq) =
     let currentIndex, setCurrentIndex = React.useState(0)
-
+(*
     React.useEffectOnce(fun () ->
         let intervalId = Browser.Dom.window.setInterval((fun () ->
             setCurrentIndex(fun prevIndex -> (prevIndex + 1) % images.Length)
         ), 3000)
         fun () -> Browser.Dom.window.clearInterval(intervalId) |> ignore
     )
-
+*)
     Html.div [
         prop.className "carousel w-full"
         prop.children [
             for image in images do
                 Html.div [
-                    prop.className (if images.[currentIndex] = image then "carousel-item block" else "carousel-item hidden")
+                    //prop.className (if images.[currentIndex] = image then "carousel-item block" else "carousel-item hidden")
                     prop.children [
                         Html.img [
                             prop.src image
