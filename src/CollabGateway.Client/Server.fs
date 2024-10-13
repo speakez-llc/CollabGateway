b4c82b8660b10951e37a222856d9bb63b37b02d4
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2024-10-12 20:08:35 -0400

    Add CORS

--- src/CollabGateway.Client/Server.fs
+++ src/CollabGateway.Client/Server.fs
@@ -1,11 +1,12 @@
 ﻿module CollabGateway.Client.Server
 
+open System
 open Fable.SimpleJson
 open Fable.Remoting.Client
 open CollabGateway.Shared.Errors
 open CollabGateway.Shared.API
 
-let private exnToError (e:exn) : ServerError =
+let private exnToError (e: exn) : ServerError =
     match e with
     | :? ProxyRequestException as ex ->
         try
@@ -14,16 +15,22 @@ let private exnToError (e:exn) : ServerError =
         with _ -> ServerError.Exception(e.Message)
     | _ -> ServerError.Exception(e.Message)
 
-type ServerResult<'a> = Result<'a,ServerError>
+type ServerResult<'a> = Result<'a, ServerError>
 
 module Cmd =
     open Elmish
-
     module OfAsync =
         let eitherAsResult fn resultMsg =
             Cmd.OfAsync.either fn () (Result.Ok >> resultMsg) (exnToError >> Result.Error >> resultMsg)
 
+
+let serverUrl =
+    match Environment.GetEnvironmentVariable("SERVER_URL") with
+    | null | "" -> "http://localhost:5000"
+    | url -> url
+
 let service =
     Remoting.createApi()
+    |> Remoting.withBaseUrl(serverUrl)
     |> Remoting.withRouteBuilder Service.RouteBuilder
     |> Remoting.buildProxy<Service>
--- src/CollabGateway.Server/Program.fs
+++ src/CollabGateway.Server/Program.fs
@@ -4,18 +4,31 @@ open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Giraffe
+open Microsoft.AspNetCore.Cors.Infrastructure
+
+let private configureCors (builder: CorsPolicyBuilder) =
+    builder.WithOrigins("http://localhost:8080", "https://rower.speakez.tech", "https://rower-stg.speakez.tech")
+           .AllowAnyHeader()
+           .AllowAnyMethod()
+           .AllowCredentials()
+    |> ignore
 
 let private configureWeb (builder: WebApplicationBuilder) =
     builder.Services.AddGiraffe() |> ignore
     builder.Services.AddLogging() |> ignore
+    builder.Services.AddCors(fun options ->
+        options.AddPolicy("AllowClient", configureCors)
+    ) |> ignore
     builder
 
 let private configureApp (app: WebApplication) =
+    app.UseCors("AllowClient") |> ignore
     app.UseStaticFiles() |> ignore
     app.UseGiraffe WebApp.webApp
     app
 
 let private builderOptions = WebApplicationOptions(WebRootPath = "public")
+
 let private builder =
     WebApplication.CreateBuilder(builderOptions)
     |> configureWeb
