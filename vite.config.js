b49911eded0f4344ea9ba7f1f2f9f29d36901ce1
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2024-10-24 20:05:09 -0400

    making VITE_BASE_URL the default

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 ﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const baseUrl = process.env.VITE_BASE_URL || 'http://localhost:5000'
-
 export default defineConfig({
-    plugins: [react({ jsxRuntime: 'classic'})],
+    plugins: [react({ jsxRuntime: 'classic' })],
     root: "./src/CollabGateway.Client",
     server: {
         port: 8080,
         host: true,
         proxy: {
-            '/api': baseUrl,
+            '/api': import.meta.env.VITE_BASE_URL || 'http://localhost:5000',
         }
     },
     build: {
