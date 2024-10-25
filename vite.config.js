7901e0ba526ec88b0927218978456439dd18e52b
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2024-10-24 20:24:02 -0400

    more context

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 ﻿import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
 
-    return defineConfig({
+    return {
         plugins: [react({ jsxRuntime: 'classic' })],
         root: "./src/CollabGateway.Client",
         server: {
@@ -17,5 +17,5 @@ export default ({ mode }) => {
         build: {
             outDir: "../../publish/app/public"
         }
-    })
-}
+    }
+})
