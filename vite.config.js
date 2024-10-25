28970ec1793bd3b912e62b6904eb5f7f828ed2f4
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2024-10-24 20:20:26 -0400

    vite using mode

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-﻿import { defineConfig } from 'vite'
+﻿import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-    plugins: [react({ jsxRuntime: 'classic' })],
-    root: "./src/CollabGateway.Client",
-    server: {
-        port: 8080,
-        host: true,
-        proxy: {
-            '/api': context.env.VITE_BASE_URL || 'http://localhost:5000',
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return defineConfig({
+        plugins: [react({ jsxRuntime: 'classic' })],
+        root: "./src/CollabGateway.Client",
+        server: {
+            port: 8080,
+            host: true,
+            proxy: {
+                '/api': env.VITE_BASE_URL || 'http://localhost:5000',
+            }
+        },
+        build: {
+            outDir: "../../publish/app/public"
         }
-    },
-    build: {
-        outDir: "../../publish/app/public"
-    }
-})
+    })
+}
