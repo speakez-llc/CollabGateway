cb6d068c72f0d350a906e1cf74f2727425cb7039
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2025-01-02 20:52:30 -0500

    update GICS reset

--- src/CollabGateway.Server/DatabaseGicsHelper.fsx
+++ src/CollabGateway.Server/DatabaseGicsHelper.fsx
@@ -1,10 +1,7 @@
-﻿#r "nuget:System"
-#r "nuget:System.IO"
-#r "nuget:System.Net.Http"
-#r "nuget:FSharp.Data, 6.4.0"
-#r "nuget:Npgsql, 6.0.0"
-#r "nuget:Marten, 7.31.0"
-#r "nuget:Newtonsoft.Json, 13.0.1"
+﻿#r "nuget: FSharp.Data"
+#r "nuget: Newtonsoft.Json"
+#r "nuget: Npgsql"
+#r "nuget: System.Net.Http"
 
 open System
 open System.Text
@@ -12,18 +9,14 @@ open System.Net.Http
 open FSharp.Data
 open Newtonsoft.Json
 open Npgsql
-open System.Threading.Tasks
 
-let execNonQueryAsync connStr commandStr =
-    task {
-        use conn = new NpgsqlConnection(connStr)
-        use cmd = new NpgsqlCommand(commandStr, conn)
-        do! conn.OpenAsync()
-        let! result = cmd.ExecuteNonQueryAsync() |> Async.AwaitTask
-        return result |> ignore
-    }
+let execNonQuery connStr commandStr =
+    use conn = new NpgsqlConnection(connStr)
+    use cmd = new NpgsqlCommand(commandStr, conn)
+    conn.Open()
+    cmd.ExecuteNonQuery() |> ignore
 
-let connStr = Environment.GetEnvironmentVariable("GATEWAY_STORE")
+let connStr =
 
 let parseDatabase (connectionString: string) =
     let parts = connectionString.Split(';')
@@ -48,7 +41,7 @@ let createGicsTaxonomyTable databaseName =
         );
         CREATE INDEX IF NOT EXISTS gics_embedding_idx ON "%s{databaseName}".public."GicsTaxonomy" USING hnsw ("Embedding" vector_cosine_ops);
     """
-    execNonQueryAsync connStr commandStr
+    execNonQuery connStr commandStr
 
 type GicsCsv = CsvProvider<"GICS.csv", HasHeaders=true, Schema="SubIndustryCode (string), SubIndustry (string), Definition (string), IndustryCode (string), Industry (string), IndustryGroupCode (string), IndustryGroup (string), SectorCode (string), Sector (string)">
 
@@ -62,64 +55,62 @@ type TextRequest = {
 }
 
 let generateVector (text: string) =
-    async {
-        use client = new HttpClient()
-        let requestUri = "http://localhost:11434/api/embed"
-        let content = new StringContent(JsonConvert.SerializeObject({ model = "granite-embedding:latest"; input = text }), Encoding.UTF8, "application/json")
-        let! response = client.PostAsync(requestUri, content) |> Async.AwaitTask
-        if not response.IsSuccessStatusCode then
-            let! errorContent = response.Content.ReadAsStringAsync() |> Async.AwaitTask
-            Console.WriteLine $"HTTP error: {response.StatusCode} - {errorContent}"
-        let! responseBody = response.Content.ReadAsStringAsync() |> Async.AwaitTask
-        try
-            let embeddingResponse = JsonConvert.DeserializeObject<EmbeddingResponse>(responseBody)
-            if embeddingResponse.embeddings = null || embeddingResponse.embeddings.Length = 0 then
-                failwith "Embedding is null or empty"
-            let vectorBody = embeddingResponse.embeddings.[0]
-            return vectorBody
-        with
-        | ex ->
-            Console.WriteLine $"Error deserializing response: {ex.Message}"
-            return Array.empty<float>
-    }
+    use client = new HttpClient()
+    let requestUri = "http://localhost:11434/api/embed"
+    let content = new StringContent(JsonConvert.SerializeObject({ model = "granite-embedding:latest"; input = text }), Encoding.UTF8, "application/json")
+    let response = client.PostAsync(requestUri, content).Result
+    if not response.IsSuccessStatusCode then
+        let errorContent = response.Content.ReadAsStringAsync().Result
+        Console.WriteLine $"HTTP error: {response.StatusCode} - {errorContent}"
+    let responseBody = response.Content.ReadAsStringAsync().Result
+    try
+        let embeddingResponse = JsonConvert.DeserializeObject<EmbeddingResponse>(responseBody)
+        if embeddingResponse.embeddings = null || embeddingResponse.embeddings.Length = 0 then
+            failwith "Embedding is null or empty"
+        embeddingResponse.embeddings.[0]
+    with
+    | ex ->
+        Console.WriteLine $"Error deserializing response: {ex.Message}"
+        Array.empty<float>
 
-let insertGicsTaxonomyAsync =
-    task {
-        let filePath = "GICS.csv"
-        let databaseName = parseDatabase connStr
-        do! createGicsTaxonomyTable databaseName
-        let truncateCommandStr = $"TRUNCATE TABLE \"%s{databaseName}\".public.\"GicsTaxonomy\";"
-        do! execNonQueryAsync connStr truncateCommandStr
-        let csv = GicsCsv.Load(filePath)
-        let rows = csv.Rows |> Seq.toArray
-        Console.WriteLine $"Inserting GicsTaxonomy table with {rows.Length} rows."
-        for index, row in rows |> Seq.mapi (fun i r -> i, r) do
-            Console.WriteLine $"Inserting row {index}"
-            let subIndustryCode = if String.IsNullOrWhiteSpace(row.SubIndustryCode) then "NULL" else $"'{row.SubIndustryCode}'"
-            let subIndustry = if String.IsNullOrWhiteSpace(row.SubIndustry) then "NULL" else $"'{row.SubIndustry}'"
-            let definition = if String.IsNullOrWhiteSpace(row.Definition) then "NULL" else $"'{row.Definition}'"
-            let industryCode = row.IndustryCode
-            let industry = row.Industry
-            let industryGroupCode = row.IndustryGroupCode
-            let industryGroup = row.IndustryGroup
-            let sectorCode = row.SectorCode
-            let sector = row.Sector
-            let textToEncode =
-                if subIndustry = "NULL" then
-                    $"{sector} {industryGroup} {industry} {definition}"
-                else
-                    $"{sector} {industryGroup} {industry} {subIndustry} {definition}"
-            try
-                let! vector = generateVector(textToEncode)
-                let vectorStr = String.Join(",", vector)
-                let commandStr = $"""
-                    INSERT INTO "%s{databaseName}".public."GicsTaxonomy"
-                    ("Id", "SubIndustryCode", "SubIndustry", "Definition", "IndustryCode", "Industry", "IndustryGroupCode", "IndustryGroup", "SectorCode", "Sector", "Embedding")
-                    VALUES ('%s{Guid.NewGuid().ToString()}', {subIndustryCode}, {subIndustry}, {definition}, '{industryCode}', '{industry}', '{industryGroupCode}', '{industryGroup}', '{sectorCode}', '{sector}', '[%s{vectorStr}]'::vector);
-                """
-                do! execNonQueryAsync connStr commandStr
-            with
-            | ex -> Console.WriteLine $"Error generating vector or inserting row: {ex.Message}"
-    }
+let insertRow (databaseName: string) (row: GicsCsv.Row) =
+    let subIndustryCode = if String.IsNullOrWhiteSpace(row.SubIndustryCode) then "NULL" else $"'{row.SubIndustryCode}'"
+    let subIndustry = if String.IsNullOrWhiteSpace(row.SubIndustry) then "NULL" else $"'{row.SubIndustry}'"
+    let definition = if String.IsNullOrWhiteSpace(row.Definition) then "NULL" else $"'{row.Definition}'"
+    let industryCode = row.IndustryCode
+    let industry = row.Industry
+    let industryGroupCode = row.IndustryGroupCode
+    let industryGroup = row.IndustryGroup
+    let sectorCode = row.SectorCode
+    let sector = row.Sector
+    let textToEncode =
+        if subIndustry = "NULL" then
+            $"{sector} {industryGroup} {industry} {definition}"
+        else
+            $"{sector} {industryGroup} {industry} {subIndustry} {definition}"
+    try
+        let vector = generateVector(textToEncode)
+        let vectorStr = String.Join(",", vector)
+        let commandStr = $"""
+            INSERT INTO "%s{databaseName}".public."GicsTaxonomy"
+            ("Id", "SubIndustryCode", "SubIndustry", "Definition", "IndustryCode", "Industry", "IndustryGroupCode", "IndustryGroup", "SectorCode", "Sector", "Embedding")
+            VALUES ('%s{Guid.NewGuid().ToString()}', {subIndustryCode}, {subIndustry}, {definition}, '{industryCode}', '{industry}', '{industryGroupCode}', '{industryGroup}', '{sectorCode}', '{sector}', '[%s{vectorStr}]'::vector);
+        """
+        execNonQuery connStr commandStr
+    with
+    | ex -> Console.WriteLine $"Error generating vector or inserting row: {ex.Message}"
 
-insertGicsTaxonomyAsync |> Async.AwaitTask |> ignore
+let insertGicsTaxonomy () =
+    let filePath = "GICS.csv"
+    let databaseName = parseDatabase(connStr)
+    createGicsTaxonomyTable(databaseName)
+    let truncateCommandStr = $"TRUNCATE TABLE \"%s{databaseName}\".public.\"GicsTaxonomy\";"
+    execNonQuery connStr truncateCommandStr
+    let csv = GicsCsv.Load(filePath)
+    let rows = csv.Rows |> Seq.toArray
+    Console.WriteLine $"Inserting GicsTaxonomy table with {rows.Length} rows."
+    for index, row in rows |> Seq.mapi (fun i r -> i, r) do
+        Console.WriteLine $"Inserting row {index}"
+        insertRow databaseName row
+
+insertGicsTaxonomy()
