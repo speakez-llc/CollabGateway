35f476de5f1647c1a6a8893533f23c82f887ae40
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2025-01-04 06:25:39 -0500

    Results *Only* Work Environment

--- src/CollabGateway.Server/samplequery.sql
+++ src/CollabGateway.Server/samplequery.sql
@@ -1,8 +1,41 @@
-﻿SELECT COUNT(DISTINCT stream_id)
-FROM mt_events
-WHERE stream_id NOT IN (
+﻿WITH streams_with_geoip AS (
+    SELECT DISTINCT stream_id
+    FROM mt_events
+    WHERE data::jsonb @> '{"Case": "GeoIPInformation"}'
+),
+streams_with_datapolicy AS (
     SELECT DISTINCT stream_id
     FROM mt_events
     WHERE data::jsonb @> '{"Case": "DataPolicyAcceptButtonClicked"}'
-   OR data::jsonb @> '{"Case": "DataPolicyDeclinedButtonClicked"}'
-    );
+       OR data::jsonb @> '{"Case": "DataPolicyDeclinedButtonClicked"}'
+    AND is_archived = false
+)
+SELECT COUNT(DISTINCT stream_id) AS stream_count
+FROM streams_with_geoip
+WHERE stream_id NOT IN (SELECT stream_id FROM streams_with_datapolicy);
+
+/* The following query is a bit more complex, but still straightforward.
+   It counts the number of active streams that have GeoIP information, but do
+   not have a Data Policy decision (human action). It also groups the results
+   by the ASN (Autonomous System Number), ASN 'type' and orders them. */
+WITH streams_with_datapolicy AS (
+    SELECT DISTINCT stream_id
+    FROM mt_events
+    WHERE data::jsonb @> '{"Case": "DataPolicyAcceptButtonClicked"}'
+       OR data::jsonb @> '{"Case": "DataPolicyDeclinedButtonClicked"}'
+),
+active_streams_without_datapolicy AS (
+    SELECT DISTINCT stream_id
+    FROM mt_events
+    WHERE stream_id NOT IN (SELECT stream_id FROM streams_with_datapolicy)
+      AND is_archived = false
+)
+SELECT data->'Fields'->0->'UserGeoInfo'->'as'->>'asn' AS ASnumber,
+       data->'Fields'->0->'UserGeoInfo'->>'isp' AS ISP,
+       data->'Fields'->0->'UserGeoInfo'->'as'->>'type' AS ASNtype,
+       COUNT(DISTINCT stream_id) AS stream_count
+FROM mt_events
+WHERE stream_id IN (SELECT stream_id FROM active_streams_without_datapolicy)
+  AND data->'Fields'->0->'UserGeoInfo'->'as'->>'asn' IS NOT NULL
+GROUP BY ASnumber, ISP, ASNtype
+ORDER BY stream_count DESC;
